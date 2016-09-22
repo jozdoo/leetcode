5175967a0c76316149850d1603f4bb458bdb7ae1
jozdoo <jozdoo@gmail.com>
2016-09-22 21:52:42 +0800

    todo RegularExpressionMatching10

---
+++ src/main/java/com/jozdoo/leetcode/RegularExpressionMatching10.java
@@ -0,0 +1,29 @@
+package com.jozdoo.leetcode;
+
+/**
+ * @author jozdoo
+ * @company 杭州尚尚签网络科技有限公司
+ * @date 16/9/22
+ * @since 3.0
+ *        Implement regular expression matching with support for '.' and '*'.
+ *        '.' Matches any single character.
+ *        '*' Matches zero or more of the preceding element.
+ *        The matching should cover the entire input string (not partial).
+ *        The function prototype should be:
+ *        bool isMatch(const char *s, const char *p)
+ *        Some examples:
+ *        isMatch("aa","a") → false
+ *        isMatch("aa","aa") → true
+ *        isMatch("aaa","aa") → false
+ *        isMatch("aa", "a*") → true
+ *        isMatch("aa", ".*") → true
+ *        isMatch("ab", ".*") → true
+ *        isMatch("aab", "c*a*b") → true
+ *
+ *        todo 16/9/29
+ */
+public class RegularExpressionMatching10 {
+    public boolean isMatch(String s, String p) {
+        return true;
+    }
+}
