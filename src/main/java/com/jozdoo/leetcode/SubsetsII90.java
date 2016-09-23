45c00ab6f5118a56b10401fcd92c399edb9a7bbb
jozdoo <jozdoo@gmail.com>
2016-09-23 09:40:16 +0800

    SubsetsII90 todo 16/9/30

---
+++ src/main/java/com/jozdoo/leetcode/SubsetsII90.java
@@ -0,0 +1,30 @@
+package com.jozdoo.leetcode;
+
+import java.util.List;
+
+/**
+ * @author jozdoo
+ * @company 杭州尚尚签网络科技有限公司
+ * @date 16/9/23
+ * @since 3.0
+ *        Given a collection of integers that might contain duplicates, nums, return all possible subsets.
+ *        Note: The solution set must not contain duplicate subsets.
+ *        For example,
+ *        If nums = [1,2,2], a solution is:
+ *        [
+ *        [2],
+ *        [1],
+ *        [1,2,2],
+ *        [2,2],
+ *        [1,2],
+ *        []
+ *        ]
+ *
+ *        todo 16/9/30
+ *
+ */
+public class SubsetsII90 {
+    public List<List<Integer>> subsetsWithDup(int[] nums) {
+        return null;
+    }
+}
